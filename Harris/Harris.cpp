e4262f370623a405fa48b73fc4018b5d93a376f0
Dawn Yue <1179348863@qq.com>
2020-04-20 13:59:49 +0800

    课前准备

--- Harris/Harris.cpp
+++ Harris/Harris.cpp
@@ -1,20 +1,163 @@
-﻿// Harris.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <opencv2\opencv.hpp>
+#include <vector>
+using namespace cv;
+using namespace std;
+//课前准备
+/*
+*description:
+*	比较s1和s2，比较结果写入d
+*/
+void compareMat(Mat& s1, Mat& s2, Mat& d)
+{
+	if (d.empty())
+	{
+		d.create(s1.rows, s1.cols, s1.type());
+	}
 
-#include <iostream>
+	for (int y = 0; y < s1.rows; y++)
+	{
+		for (int x = 0; x < s1.cols; x++)
+		{
+			if (s1.at<float>(y, x) == s2.at<float>(y, x))
+			{
+				d.at<float>(y, x) = s1.at<float>(y, x);
+			}
+			else
+			{
+				d.at<float>(y, x) = 0.0f;
+			}
+		}
+	}
+}
+
+
+void Harris(vector<Point2i>& corners, Mat& I)
+{
+
+	if (!I.data)
+	{
+		return;
+	}
+
+	double GaussKernel[5][5] = {
+		0.00390625, 0.015625, 0.0234375, 0.015625, 0.00390625,
+		0.015625, 0.0625, 0.09375, 0.0625, 0.015625,
+		0.0234375, 0.09375, 0.140625, 0.09375, 0.0234375,
+		0.015625, 0.0625, 0.09375, 0.0625, 0.015625,
+		0.00390625, 0.015625, 0.0234375, 0.015625, 0.00390625,
+	};
+
+	Mat gray;
+	cvtColor(I, gray, CV_BGR2GRAY);
+
+	Mat IxIx(gray.rows, gray.cols, CV_8UC1);
+	Mat IyIy(gray.rows, gray.cols, CV_8UC1);
+	Mat IxIy(gray.rows, gray.cols, CV_8UC1);
+	for (int y = 1; y <= I.rows - 2; y++)
+	{
+		//const uchar* grayRow = gray.ptr<uchar>(y);
+		for (int x = 1; x <= I.cols - 2; x++)
+		{
+			//计算当前像素在两个方向的偏导数(即梯度)
+			/*本应使用Harris卷积核进行卷积，Harris卷积核：
+			[ 0  -1  0 ]
+			[ -1  0  1 ]
+			[ 0   1  0 ]
+			此处分离计算比较简单，因此分离计算
+			*/
+			uchar ix = gray.at<uchar>(y + 1, x) - gray.at<uchar>(y - 1, x);
+			uchar iy = gray.at<uchar>(y, x + 1) - gray.at<uchar>(y, x - 1);
+
+			IxIx.at<uchar>(y, x) = ix * ix;
+			IyIy.at<uchar>(y, x) = iy * iy;
+
+			IxIy.at<uchar>(y, x) = ix * iy;
+		}
+	}
+
+	//Harris响应矩阵
+	Mat R(gray.rows, gray.cols, CV_32FC1);
+	//最大响应值
+	float maxResponse = 0.0f;
+
+	for (int y = 0; y <= I.rows - 5; y++)
+	{
+		for (int x = 0; x <= I.cols - 5; x++)
+		{
+			uchar a = 0;  /*  自相似矩阵 M：                       */
+			uchar b = 0;  /*  [ ∑g*a  ∑g*c ]   即  [ ∑g*IxIx ∑g*IxIy ]        */
+			uchar c = 0;  /*  [ ∑g*c  ∑g*b ]       [ ∑g*IxIy ∑g*IyIy ]        */
+			for (int m = 0; m < 5; m++)
+			{
+				for (int n = 0; n < 5; n++)
+				{
+					//分别对IxIx，IyIy，IxIy的5*5邻域计算高斯均值以求得自相似矩阵 M
+					a += IxIx.at<uchar>(y + m, x + n) * GaussKernel[m][n];
+					b += IyIy.at<uchar>(y + m, x + n) * GaussKernel[m][n];
+					c += IxIy.at<uchar>(y + m, x + n) * GaussKernel[m][n];
+				}
+			}
+
+			//计算当前像素的Harris响应值
+			float t = (a * c - b * b) - 0.05f * (a + c) * (a + c);
+
+			R.at<float>(y, x) = t;
+
+			if (t > maxResponse)
+			{
+				maxResponse = t;
+			}
+		}
+	}
+
+	/*
+	为了求得局部极大值，先对R进行膨胀，将膨胀结果与R比较，相同的就是局部极大值
+	*/
+	Mat dilated;
+	dilate(R, dilated, Mat());
+	Mat localMax;
+	compareMat(R, dilated, localMax);
+
+	//剔除部分局部极大值，这是因为如果不提出，局部极大值有可能很多，不是想要的结果
+	Mat threshold2;
+	threshold(localMax, threshold2, 0.5 * maxResponse, 255, THRESH_BINARY);
+	//imshow("localMax", threshold2);
+
+	for (int y = 0; y < threshold2.rows - 1; y++)
+	{
+		const float* row = threshold2.ptr<float>(y);
+		for (int x = 0; x < threshold2.cols - 1; x++)
+		{
+			if (row[x])
+			{
+				corners.push_back(Point2i(x, y));
+			}
+		}
+	}
+
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	Mat m = imread("E:\\1.png");
+
+
+	vector<Point2i> corners;
+	Harris(corners, m);
+
+	for (vector<Point2i>::iterator it = corners.begin(); it != corners.end(); it++)
+	{
+		circle(m, *it, 2, Scalar(255, 242, 0));
+	}
+
+	imshow("ok", m);
+
+	waitKey();
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+//https://github.com/ooooooops/HarrisCornersDetector/blob/master/harris.cpp
